1b787e8669d8a708880ce52a1998e06a07443d43
Aminu <abymbyiby@gmail.com>
2023-03-27 19:15:47 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Takes a pointer to qn in   t and updates
+ * the value it points to to 98
+ *
+ * @n: An interger input pointer
+ *
+ * Return : nothing
+ */
+void reset_to_98(int *n)
+{
+
+	an = 98;
+
+}
+
+
